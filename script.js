ef354dda3afe45f96cd29cf734b189ef261526c6
Yunovuch <uonovichkolia@gmail.com>
2025-03-14 00:57:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ document.getElementById("reservation-form").addEventListener("submit", function(
     let form = this;
     let formData = new FormData(form);
 
+    let phonePattern = /^[0-9]{10,15}$/;
+    if (!phonePattern.test(document.getElementById("phone").value)) {
+        alert("Некоректний номер телефону!");
+        return;
+    }
+
     fetch("send_mail.php", {
         method: "POST",
         body: formData
     })
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
+        if (data.status === "error") {
+            alert(data.message);
+            return;
+        }
         document.getElementById("success-message").style.display = "block";  // Показуємо повідомлення
         form.reset();  // Очищаємо форму
     })
